98317d1148bc5398e1077076695ac56884fa15bc
mika7028 <mika7028@gmail.com>
2017-05-21 15:36:33 +0900

    つぶやきアプリ
    ログインしてつぶやけるＷＥＢアプリケーションの作成

---
+++ Login.java
@@ -0,0 +1,71 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.LoginLogic;
+import model.User;
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, 
+			HttpServletResponse response) 
+					throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//リクエストパラメータの取得
+		request.setCharacterEncoding("UTF-8");
+		String name = request.getParameter("name");
+		String pass = request.getParameter("pass");
+		
+		//Userインスタンスの作成
+		User user= new User(name, pass);
+		
+		//ログイン情報
+		LoginLogic loginLogic = new LoginLogic();
+		boolean isLogin = loginLogic.execute(user);
+		
+		//ログイン成功時の処理
+		if(isLogin){
+			
+			//ユーザー情報をセッションスコープに保存
+			HttpSession session = request.getSession();
+			session.setAttribute("loginUser", user);
+		}
+		//ログイン結果画面にフォワード
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/loginResult.jp");
+		dispatcher.forward(request, response);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

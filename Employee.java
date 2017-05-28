529a05acd0d1fd31bb57873ad27a78b8b3c2fc48
mika7028 <mika7028@gmail.com>
2017-05-28 19:58:00 +0900

    DAOパターンのサンプルプログラム

--- Employee.java
+++ Employee.java
@@ -1,11 +1,5 @@
 package model;
 
 public class Employee {
-	private String id;
-	private String name;
-	public Employee(String id, String name){
-		this.id = id; this.name = name;
-	}
-	public String getID(){return id;}
-	public String getName(){return name;}
+
 }
